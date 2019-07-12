86cd888f0687f20a03276fe9318509fd00458b0b
Roberson Carlos <robersonfox@gmail.com>
2019-07-12 12:17:00 -0300

    Second Push
    Configuração do banco de dados

---
+++ src/main/java/br/com/robersonfox/security/jwtsecurity/controller/TokenController.java
@@ -0,0 +1,28 @@
+package br.com.robersonfox.security.jwtsecurity.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.robersonfox.security.jwtsecurity.model.JwtUser;
+import br.com.robersonfox.security.jwtsecurity.security.JwtGenerator;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
